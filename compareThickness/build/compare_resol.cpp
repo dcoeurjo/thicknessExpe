e1260a56810db7c63df917ba63b628a5c21e7092
WilliamAufort <william.aufort@ens-lyon.fr>
2014-06-18 10:02:28 +0200

    tools to compare thickness distributions

---
+++ compareThickness/build/compare_resol.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+/*------------------------------------------*\
+| Create the data used to plot the thickness |
+|   diagram computed by the volumic method.  |
+\*------------------------------------------*/
+
+
+int main ( int argc, char * argv[] ) {
+  // argv[1] = name of the file ( type : name-resolution )
+  // argv[2] = number of faces ( in sdf computation )
+  // argv[3] = maximal thickness in argv[1].off
+  // argv[4] = number of voxels in argv[1].off
+  // These 3 arguments are used in the change of scale
+  // to plot all the distributions
+  double max = atof(argv[3]) ;
+  double factor = atof(argv[2]) / atof(argv[4]) ;
+  string input = argv[1] ;
+  string fileInput = input+"-data.txt" ; 
+  string fileOutput = input+"-newdata.txt" ;
+  ifstream file1( fileInput.c_str(), ios::in); 
+  ofstream file2( fileOutput.c_str(), ios::out);
+  if ( file1 ) {
+      if ( file2 ) {
+          file2 << "x thickness_distribution" << std::endl ;
+          file2 << "0 0" << std::endl ;
+          string line ; 
+          double i = 1 ;
+          while ( getline(file1,line) ) {
+              double value = atof(line.c_str())/ max ;
+              double x = i * factor ;
+              file2 << x << " " << value << endl ;
+              i++ ;
+          }
+	  file1.close() ;
+          file2.close() ;
+          return 0 ;
+      }
+      else
+      cerr << "write file error" << endl ;
+      return -1 ;
+  }
+  else
+  cerr << "open file error" << endl ;
+  return -1 ;
+}
---
+++ compareThickness/build/profil_vol.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OFFNAME=$1
+~/Documents/off2vol.sh $OFFNAME $2
+VOLNAME=${OFFNAME%%.off}-$2.vol
+FILENAME=${OFFNAME%%.off}-$2-data.txt
+thicknessComputationApproxLinear -i $VOLNAME -o tmp.longvol > $FILENAME
+rm tmp.longvol 
+rm $VOLNAME
+
+SDFFILE=${OFFNAME%%.off}-sdf.txt
+FACES=$(wc -l $SDFFILE | cut -d" " -f1)
+FACES=$(($FACES-2))  # because of the header of the .off file
+MAX=$(tail -1 $FILENAME)
+VOXELS=$(wc -l $FILENAME | cut -d" " -f1)
+
+./compare_resol ${OFFNAME%%.off}-$2 $FACES $MAX $VOXELS 
+rm $FILENAME

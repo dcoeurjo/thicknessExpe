b0acb24e84da4a7c31e2adf279190809f3dcd37b
WilliamAufort <william.aufort@ens-lyon.fr>
2014-06-27 16:54:20 +0200

    Calcul de l'erreur locale volumique : ajout fichier, readme incomplet

---
+++ localErrorVol/localErrorVol.cpp
@@ -0,0 +1,402 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <math.h>
+#include <cstdlib> 
+#include <algorithm>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Sphere_3.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/box_intersection_d.h>
+#include <CGAL/Bbox_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/mesh_segmentation.h>
+#include <CGAL/property_map.h>
+
+#include "spheresTools.h"
+
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel    Kernel;
+typedef CGAL::Polyhedron_3<Kernel>                             Polyhedron;
+typedef Kernel::Vector_3                                       Vector;
+typedef Kernel::Point_3                                        Point;
+typedef Polyhedron::Vertex                                     Vertex;
+typedef Kernel::Sphere_3                                       Sphere;
+typedef std::vector<Sphere>                                    Spheres;
+typedef Spheres::iterator                                      Iterator; 
+typedef CGAL::Box_intersection_d::Box_with_handle_d<double,3,Iterator>  Box ;
+
+typedef Polyhedron::Facet_iterator                             Facet_iterator;
+typedef Polyhedron::Vertex_iterator                            Vertex_iterator;
+typedef Polyhedron::Halfedge_around_facet_circulator           HF_circulator;
+typedef Polyhedron::Halfedge_around_vertex_const_circulator    HV_circulator;
+ 
+
+
+/**********************************************\
+|  Volumetric method robustness against noise  |
+\**********************************************/
+
+// Here we compare the influence of the resolution of the volumic image.
+// If we want to consider the magnitude of the noise, we have to modify
+// few lines of the code.
+
+
+int main ( int argc, char * argv[] ) {
+    // argv[1] = filename ( type .off )
+    // argv[2] = resolutions 
+    // argv[3] = type of noise (1,2,3)
+    // argv[4] = magnitude of noise 
+     
+    // get the resolutions
+    std::vector<int> resolutions = getResolutions(argv[2]) ;
+    // build the off noised mesh
+    std::string offName = argv[1] ; 
+    std::string noisedOffName = offName.substr(0,offName.size()-4)+"-noised.off" ;
+    int s = createNoiseOff(argv[1], noisedOffName.c_str(),atof(argv[4]),atoi(argv[3])) ;
+    if ( s != 0 ) {
+        std::cerr << "Error when creating the noised mesh " << std::endl ;
+        return -1 ;
+    }
+    // init script
+    std::string scriptName = offName.substr(0,offName.size()-4)+"-script.p" ;
+    std::ofstream scriptFile(scriptName.c_str(),std::ios::out) ;
+    if ( !scriptFile ) {
+        std::cerr << "Open script file error" << std::endl ;
+        return -1 ;
+    }  
+    scriptFile << "reset " << std::endl ;
+    scriptFile << std::endl ;
+    scriptFile << "set term pdfcairo" << std::endl ;
+    scriptFile << "set output \"" << argv[1] << "-local-error.pdf\"" << std::endl ;
+    scriptFile << "set title \" Distribution of the local error \" " << std::endl ;
+    scriptFile << "set xlabel \" Voxels (%) \" " << std::endl ;
+    scriptFile << "set ylabel \" Local error (%) \" " << std::endl ;
+    scriptFile << std::endl ;
+    scriptFile << "set style data line " << std::endl ;
+    scriptFile << "set pointsize 2 " << std::endl ; 
+    scriptFile << "set key bottom right " << std::endl ;
+    scriptFile << std::endl ;
+    scriptFile << "plot " ;
+    std::cout << "Script ok !" << std::endl ;
+    // init arrays
+
+    // main loop //
+    unsigned int i ;
+    for ( i = 0 ; i < resolutions.size() ; i++ ) {
+        Calcul calc(resolutions[i]) ; 
+        std::vector<double> thickWithoutNoise = offToThick(offName,resolutions[i],calc) ;
+        initMaxRadius(resolutions[i]) ; // maxRadius = 0 for all points
+        std::vector<double> thickWithNoise = offToThick(noisedOffName.c_str(),resolutions[i],calc) ;
+        freeArrays() ;
+        // Note : thickWithoutNoise.size() == thickWithNoise.size()) == resolution^3
+        std::string fileResults = noisedOffName.substr(0,noisedOffName.size()-4)+"-"+doubleToString(resolutions[i])+"-local-error.txt";
+        std::ofstream fichier(fileResults.c_str(), std::ios::out);
+        if( !fichier ){ 
+            std::cout << " Error in creating file " << fileResults << std::endl;
+            return -1 ;
+        } 
+        // Compute the local error
+        std::cout << "Calculations... " ;
+        int j = 0 ;
+        int size = resolutions[i] * resolutions[i] * resolutions[i] ;
+        std::vector<double> localError ;
+        for(j=0;j<size;j++) {
+             // We consider the points which belong to the two meshes
+             if ((thickWithoutNoise[j] != 0) && (thickWithNoise[j] != 0)) {
+                 localError.push_back(error(thickWithoutNoise[j],thickWithNoise[j]));
+             }
+        }; 
+        std::cout << "Ok" << std::endl << "Sort ... " ;
+        // Sort the values 
+        std::sort(localError.begin(),localError.end());
+        // Put in file
+        std::cout << "Ok" << std::endl << "Write in files... " ;
+        int taille = localError.size() ;
+        for(j=0;j<taille;j++) {
+            fichier << (j+1)*100.0/( (float) taille) << " " << localError[j] << std::endl ;
+        };
+        scriptFile << "\""+fileResults+"\" using 1:2 title \"resol = "+doubleToString(resolutions[i])+"\", \\" 
+        << std::endl ;
+        std::cout << "Ok" << std::endl;
+    };
+    return 0 ;
+}
+
+
+
+/********************\
+| AUXILIAR FUNCTIONS |
+\********************/
+
+
+/************\
+| Converters |
+\************/
+
+std::string doubleToString(double number) {
+   std::ostringstream ss;
+   ss << number;
+   return ss.str();
+}
+
+double stringToDouble ( const std::string &s ) {
+    std::stringstream convert(s) ;
+    double number ;
+    convert >> number ;
+    return number ;
+}
+
+
+
+/**************\
+| Sphere tools |
+\**************/
+
+
+std::vector<Sphere> readSpheres ( const char * filename ) {
+    std::vector<Sphere> spheres ;
+    std::ifstream input(filename) ;
+    if (!input) {
+        std::cerr << " Open sphere file error " << std::endl ;
+    }
+    std::string number ;
+    while(input >> number) {
+        double x = stringToDouble(number) ;
+        if ( input >> number ) {
+            double y = stringToDouble(number) ;
+            if ( input >> number ) {        
+                double z = stringToDouble(number) ;
+                if ( input >> number ) {
+                    double r = stringToDouble(number) ;
+                    Point P(x,y,z) ;
+                    Sphere S(P,r*r) ;
+                    spheres.push_back(S) ;
+                }
+                else std::cerr << " Incorrect sphere file : cannot read R " << std::endl ;
+            }
+            else std::cerr << " Incorrect sphere file : cannot read z " << std::endl ;            
+        }
+        else std::cerr << " Incorrect sphere file : cannot read y " << std::endl ;
+    }
+    return spheres ;
+}
+
+
+
+/***********************\
+| Tools to create noise |
+\***********************/
+
+std::vector<double> findBoxBorder ( Polyhedron P ) {
+  Vertex_iterator v = P.vertices_begin() ;
+  std::vector<double> borders(6) ;
+    borders[0] = v->point().x() ;
+    borders[1] = v->point().x() ;
+    borders[2] = v->point().y() ;
+    borders[3] = v->point().y() ;
+    borders[4] = v->point().z() ;
+    borders[5] = v->point().z() ;
+  do {
+      double x = v->point().x() ;
+      double y = v->point().y() ;
+      double z = v->point().z() ; 
+      if ( x < borders[0] ) {
+          borders[0] = x ;
+      }
+      else if ( x > borders[1] ) {
+          borders[1] = x ;
+      }
+      if ( y < borders[2] ) {
+          borders[2] = y ;
+      }
+      else if ( y > borders[3] ) {
+          borders[3] = y ;
+      }
+      if ( z < borders[4] ) {
+          borders[4] = z ;
+      }
+      else if ( z > borders[5] ) {
+          borders[5] = z ;
+      }
+  } while ( v++ != P.vertices_end() ) ;
+   return borders ;
+}
+
+
+double longestDiagonalLength ( Polyhedron P ) {
+  std::vector<double> borders = findBoxBorder(P) ;
+  double deltaX = borders[1]-borders[0] ;
+  double deltaY = borders[3]-borders[2] ;
+  double deltaZ = borders[5]-borders[4] ;
+  return sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ) ;
+}
+
+int createNoiseOff ( char * fileName, const char * outputName, double magnitude, int option) {    
+    // Create and read Polyhedron
+    Polyhedron mesh ;
+    std::ifstream input(fileName) ;
+    if ( !input || !(input >> mesh) || mesh.empty() ) {
+        std::cerr << "Not a valid .off file" << std::endl; 
+    };
+    // Initialize generators
+    std::default_random_engine generator;
+    double s = magnitude/100*longestDiagonalLength(mesh);
+    std::normal_distribution<double> distribution(0.0,s); 
+    // apply noise forall vertices
+    switch ( option ) {
+        case 1 : { // "cubic noise"
+            Vertex_iterator v = mesh.vertices_begin() ;
+            do {
+                double x = distribution(generator);
+                double y = distribution(generator);
+                double z = distribution(generator);
+                Vector vec(x,y,z);
+                v->point() = v->point() + vec ;
+                } while ( v++ != mesh.vertices_end()) ;
+        break ;
+        }
+        case 2 : { // "spheric noise"
+            Vertex_iterator v = mesh.vertices_begin() ;
+            do {
+                bool inCircle = false ;
+                double x, y, z ;
+                while(!inCircle) {
+                     x = distribution(generator);
+                     y = distribution(generator);
+                     z = distribution(generator);
+                     if ( x*x + y*y + z*z <= s*s) {
+                         inCircle = true ;
+                     }
+                }
+                Vector vec(x,y,z);
+                v->point() = v->point() + vec ;
+                } while ( v++ != mesh.vertices_end()) ;
+        break ;
+        }
+        case 3 : { // "normal noise"
+            Vertex_iterator v = mesh.vertices_begin() ;
+            do { 
+                HV_circulator h = v->vertex_begin() ;
+                Vector vec(0.0,0.0,0.0) ;
+                do {
+                    vec = vec + CGAL::cross_product(  
+                                h->next()->vertex()->point() - h->vertex()->point(),
+                                h->next()->next()->vertex()->point() - h->next()->vertex()->point()) ;
+                    ++h ;
+                    CGAL_assertion( h != v.vertex_begin()); // even degree guaranteed 
+                    ++h ;
+                   } while ( h != v->vertex_begin());
+               double norme = sqrt(vec.x()*vec.x() + vec.y()*vec.y() + vec.z()*vec.z()) ;
+               double factor = distribution(generator) ; 
+               Vector vectorNoise( vec.x()*factor/norme, vec.y()*factor/norme, vec.z()*factor/norme) ;
+               v->point() = v->point() + vectorNoise ;
+               v++;
+               } while ( v != mesh.vertices_end()) ;
+        break ;
+        }
+        default : {
+          std::cerr << " Erreur de saisie " << std::endl ;
+        }
+    }; 
+    // Write polyhedron in .off format.
+    std::ofstream offOutput(outputName,std::ios::out) ;
+    CGAL::set_ascii_mode( offOutput ) ;
+    offOutput << "OFF" << std::endl ;
+    offOutput << mesh.size_of_vertices() << ' ' << mesh.size_of_facets() << ' ' << mesh.size_of_halfedges() << std::endl ; 
+    std::copy( mesh.points_begin(), mesh.points_end(), std::ostream_iterator<Point>( offOutput, "\n"));
+    for ( Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i) {
+         HF_circulator j = i->facet_begin();
+	 // facets in polyhedral surfaces are at least triangles.
+	 CGAL_assertion( CGAL::circulator_size(j) >= 3);
+	 offOutput << CGAL::circulator_size(j) << ' ' << std::distance(mesh.vertices_begin(), 
+         i->facet_begin()->vertex()) ;
+         while ( ++j != i->facet_begin()) {
+	     offOutput << ' ' << std::distance(mesh.vertices_begin(), j->vertex());
+         } 
+	 offOutput << std::endl;
+    }
+    return 0;
+}
+
+/****************************************\
+| Thickness and local error calculations |
+\****************************************/
+
+std::vector<int> getResolutions ( char * resolutionFile ) {
+  std::vector<int> resolutions ; 
+  std::ifstream file(resolutionFile,std::ios::in);
+  if (!file) {
+      std::cerr << "Error: open resolution file" << std::endl ;
+  }
+  else {
+      std::string line ;
+      while (getline(file,line)) {
+          int resol = atoi(line.c_str()) ;
+          resolutions.push_back(resol) ;
+      }
+  }
+  file.close() ;
+  sort(resolutions.begin(),resolutions.end()) ;
+  return resolutions ;
+}
+
+float error( float a, float b ) {
+      return std::abs(a-b)/a ;
+}
+
+
+
+std::vector<double> offToThick(std::string filename, int resolution, Calcul& calc ) {
+    std::string offName = filename ;
+    std::string systemCall = "bash off2spheres.sh "+offName+" "+doubleToString(resolution) ;
+    int s = system(systemCall.c_str()) ;
+    if ( s != 0 ) {
+        std::cerr << "Bash script error" << std::endl ;
+    }
+    std::string spheresFile = filename.substr(0,filename.size()-4)+"-"+doubleToString(resolution)+"-spheres.txt" ;
+    std::vector<double> thick = spheresToThick(spheresFile.c_str(),resolution,0,calc) ;
+    return thick ;
+}
+
+
+std::vector<double> spheresToThick( const char * filename , int resolution , int option , Calcul& calc ) {
+    std::vector<Sphere> spheres = readSpheres(filename) ;
+    std::vector<Box> boxes, pointBoxes ;
+    std::cout << "Building boxes ... " ;
+    for ( Iterator i = spheres.begin(); i != spheres.end(); ++i ) {
+        boxes.push_back( Box(i->bbox(),i)) ;
+    }
+    for ( Iterator j = points.begin(); j != points.end(); ++j) {
+      pointBoxes.push_back( Box(j->bbox(),j)) ;
+    }
+    std::cout << "Ok" << std::endl; 
+    std::cout << "Compute intersections ... " ;
+    CGAL::box_intersection_d( boxes.begin(), boxes.end(), pointBoxes.begin(), pointBoxes.end(), calc);
+    std::cout << "Ok" << std::endl ;
+    return maxRadius ;
+}
+
+/******************\
+| Arrays functions |
+\******************/
+
+
+void initMaxRadius(int resolution) {
+    int i ;
+    for ( i = 0 ; i < resolution * resolution * resolution ; i++) {
+        maxRadius[i] = 0 ;
+    }
+}
+
+void freeArrays( void ) {
+    maxRadius.resize(0) ; 
+    points.resize(0) ;
+}
+
+
+
+
+
---
+++ localErrorVol/spheresTools.h
@@ -0,0 +1,120 @@
+#ifndef SPHERETOOLS_H_INCLUDED
+#define SPHERETOOLS_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <math.h>
+#include <cstdlib> 
+#include <algorithm>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Sphere_3.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/box_intersection_d.h>
+#include <CGAL/Bbox_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/mesh_segmentation.h>
+#include <CGAL/property_map.h>
+
+#include "spheresTools.h"
+
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel    Kernel;
+typedef CGAL::Polyhedron_3<Kernel>                             Polyhedron;
+typedef Kernel::Vector_3                                       Vector;
+typedef Kernel::Point_3                                        Point;
+typedef Polyhedron::Vertex                                     Vertex;
+typedef Kernel::Sphere_3                                       Sphere;
+typedef std::vector<Sphere>                                    Spheres;
+typedef Spheres::iterator                                      Iterator; 
+typedef CGAL::Box_intersection_d::Box_with_handle_d<double,3,Iterator>  Box ;
+
+typedef Polyhedron::Facet_iterator                             Facet_iterator;
+typedef Polyhedron::Vertex_iterator                            Vertex_iterator;
+typedef Polyhedron::Halfedge_around_facet_circulator           HF_circulator;
+typedef Polyhedron::Halfedge_around_vertex_const_circulator    HV_circulator;
+
+Spheres points ;
+std::vector<double> maxRadius ;
+
+class Calcul {
+public:
+
+    // init points and maxRadius
+
+  Calcul(int resolution) {
+      double i, j, k ;
+      for ( k = 0 ; k < resolution ; k++ ) { 
+          for ( j = 0 ; j < resolution ; j++ ) {
+              for ( i = 0 ; i < resolution ; i++) {
+                  points.push_back(Sphere(Point(i,j,k),0)) ;
+                  maxRadius.push_back(0.0) ;
+              }
+          }
+      }
+  }
+
+
+  bool belongsTo ( const Sphere& s1, const Sphere& s2 ) {
+      Point p = s1.center() ;
+      Point q = s2.center() ;
+      double x = p.x() - q.x() ;
+      double y = p.y() - q.y() ;
+      double z = p.z() - q.z() ;
+      return ( x*x+y*y+z*z <= s1.squared_radius() ) ;
+  }
+      
+  // update the radius values  
+
+  void operator()(const Box& a, const Box& b) {
+      // b is a point
+      if ( belongsTo( *(a.handle()), *(b.handle())) 
+      && ((a.handle())->squared_radius() > maxRadius[b.handle()-points.begin()])) {
+          maxRadius[b.handle()-points.begin()] = (a.handle())->squared_radius() ;
+      }
+  }
+
+};
+
+
+/************\
+| Converters |
+\************/
+
+std::string doubleToString(double number);
+double stringToDouble ( const std::string &s );
+
+/**************\
+| Sphere tools |
+\**************/
+
+std::vector<Sphere> readSpheres ( const char * filename );
+
+/***********************\
+| Tools to create noise |
+\***********************/
+
+std::vector<double> findBoxBorder ( Polyhedron P );
+double longestDiagonalLength ( Polyhedron P );
+int createNoiseOff ( char * fileName, const char * outputName, double magnitude, int option);
+
+/****************************************\
+| thickness and local error calculations |
+\****************************************/
+
+std::vector<int> getResolutions ( char * resolutionFile );
+float error( float a, float b );
+std::vector<double> offToThick(std::string filename, int resolution, Calcul& calc );
+std::vector<double> spheresToThick( const char * filename , int resolution , int option, Calcul& calc );
+
+/******************\
+| Arrays functions |
+\******************/
+
+void initMaxRadius(int resolution);
+void freeArrays( void ) ;
+
+
+#endif 
